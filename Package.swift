f59d882432f30635eb39c407525a02bb383f53c2
Kristof Kalai <kristof.kalai@supercharge.io>
2023-03-12 17:02:21 +0100

    feat: update dependency

--- Package.swift
+++ Package.swift
@@ -15,7 +15,7 @@ let package = Package(
     dependencies: [
         // Dependencies declare other packages that this package depends on.
         // .package(url: /* package url */, from: "1.0.0"),
-        .package(url: "https://github.com/stateman92/ExceptionCatcher", exact: .init(2, 0, 7))
+        .package(url: "https://github.com/stateman92/ExceptionCatcher", exact: .init(2, 0, 8))
     ],
     targets: [
         // Targets are the basic building blocks of a package. A target can define a module or a test suite.
